e23ee65d3d514127c297e2923c9dd57fc153f125
KhoaDQ <quockhoathcstq@gmail.com>
2021-07-04 23:51:18 +0700

    add swagger

--- eShopSolution.BackendAPI/Startup.cs
+++ eShopSolution.BackendAPI/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,10 +33,15 @@ namespace eShopSolution.BackendAPI
 
             // Declare DI
             // Mỗi lần request object thì sẽ tạo mới 
-            services.AddTransient<IPublicProductService, PublicProductService>();
-
+            services.AddTransient<IPublicProductService, PublicProductService>();            
 
             services.AddControllersWithViews();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                { Title = "Swagger eShopSolution", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +63,13 @@ namespace eShopSolution.BackendAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json","Swagger eShopSolution V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
