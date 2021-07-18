bd1efbbb924bc0eab357048b195d5f3c2ac11ea3
KhoaDQ <quockhoathcstq@gmail.com>
2021-07-18 14:20:35 +0700

    add feature delete user

--- eShopSolution.AdminApp/Services/IUserApiClient.cs
+++ eShopSolution.AdminApp/Services/IUserApiClient.cs
@@ -18,5 +18,7 @@ namespace eShopSolution.AdminApp.Services
         Task<ApiResult<bool>> UpdateUser(Guid id, UserUpdateRequest request);
 
         Task<ApiResult<UserViewModel>> GetById(Guid id);
+
+        Task<ApiResult<bool>> Delete(Guid id);
     }
 }
