4a311bb58ad3792cfaf212f1d94103eb101d7bff
KhoaDQ <quockhoathcstq@gmail.com>
2021-07-21 11:13:00 +0700

    display list of product

---
+++ eShopSolution.AdminApp/Services/ProductApiClient.cs
@@ -0,0 +1,39 @@
+﻿using eShopSolution.ViewModels.Catalog.Products;
+using eShopSolution.ViewModels.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace eShopSolution.AdminApp.Services
+{
+    public class ProductApiClient : BaseApiClient, IProductApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ProductApiClient(IHttpClientFactory httpClientFactory, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
+            : base(httpClientFactory, httpContextAccessor, configuration)
+        {
+            _configuration = configuration;
+            _httpClientFactory = httpClientFactory;
+
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<PagedResult<ProductViewModel>> GetPagings(GetManageProductPagingRequest request)
+        {
+            var data = await base.GetAsync<PagedResult<ProductViewModel>>(
+                $"/api/products/paging?pageIndex={request.PageIndex}" +
+                $"&pageSize={request.PageSize}" +
+                $"&keyword={request.Keyword}&languageId={ request.LanguageId}");
+
+            return data;
+        }
+    }
+}
